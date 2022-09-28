19cd7024bc5edef8cf442ddd32a3dfafbb69acdc
Sabta Mendes Alencar <sabtaamendes@gmail.com>
2022-09-28 18:43:50 -0400

    ajuste no layout dos botoes de erro, acerto e quase acerto

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,109 +1,109 @@
 import styled from "styled-components";
 import { useState } from "react";
 export default function Card({ perguntas, index }) {
-    const [cartaoFechado, setCartaoFechado] = useState(true);
-    const [mostrarPergunta, setMostrarPergunta] = useState(false);
-    const [mostrarResposta, setMostrarResposta] = useState(false);
-    const [imagem, setImagem] = useState("");
-    const [corVermelho, setCorVermelho] = useState(false);
-    const [corLaranja, setCorLaranja] = useState(false);
-    const [corVerde, setCorVerde] = useState(false);
-    const [contador, setContador] = useState(0);
-    function showTheQuestion(index) {
-        setMostrarPergunta(true);
-        setCartaoFechado(false);
-        console.log(perguntas.Q, index)
-    }
-    function showTheAnswer(index) {
-        console.log(perguntas.Q, index)
-        setMostrarResposta(true);
-        setMostrarPergunta(false);
-        setCartaoFechado(false);
-    }
-    function showCardAnswerRed() {
-        const cartoesRespondidos = contador + 1;
-        setMostrarResposta(false);
-        setCorVermelho(true);
-        setContador(cartoesRespondidos);
-        setImagem(perguntas.erro);
-        console.log("Clicado");
-    }
-    function showCardAnswerOrange() {
-        const cartoesRespondidos = contador + 1;
-        setMostrarResposta(false);
-        setCorLaranja(true);
-        setImagem(perguntas.quase);
-        setContador(cartoesRespondidos);
-    }
-    function showCardAnswerGreen() {
-        const imagemCerto = perguntas.certo;
-        console.log(imagemCerto)
-        const cartoesRespondidos = contador + 1;
-        setMostrarResposta(false);
-        setCorVerde(true);
-        setImagem(imagemCerto);
-        setContador(cartoesRespondidos);
-    }
-    return (
-        <>
-            {cartaoFechado &&
-                <PerguntaFechada>
-                    <p>{perguntas.pergunta}</p>
-                    <img onClick={() => showTheQuestion(index)} src={perguntas.autoplay}
-                        alt={perguntas.alt} />
-                </PerguntaFechada>
-            }
+  const [cartaoFechado, setCartaoFechado] = useState(true);
+  const [mostrarPergunta, setMostrarPergunta] = useState(false);
+  const [mostrarResposta, setMostrarResposta] = useState(false);
+  const [imagem, setImagem] = useState("");
+  const [corVermelho, setCorVermelho] = useState(false);
+  const [corLaranja, setCorLaranja] = useState(false);
+  const [corVerde, setCorVerde] = useState(false);
+  const [contador, setContador] = useState(0);
+  function showTheQuestion(index) {
+    setMostrarPergunta(true);
+    setCartaoFechado(false);
+    console.log(perguntas.Q, index)
+  }
+  function showTheAnswer(index) {
+    console.log(perguntas.Q, index)
+    setMostrarResposta(true);
+    setMostrarPergunta(false);
+    setCartaoFechado(false);
+  }
+  function showCardAnswerRed() {
+    const cartoesRespondidos = contador + 1;
+    setMostrarResposta(false);
+    setCorVermelho(true);
+    setContador(cartoesRespondidos);
+    setImagem(perguntas.erro);
+    console.log("Clicado");
+  }
+  function showCardAnswerOrange() {
+    const cartoesRespondidos = contador + 1;
+    setMostrarResposta(false);
+    setCorLaranja(true);
+    setImagem(perguntas.quase);
+    setContador(cartoesRespondidos);
+  }
+  function showCardAnswerGreen() {
+    const imagemCerto = perguntas.certo;
+    console.log(imagemCerto)
+    const cartoesRespondidos = contador + 1;
+    setMostrarResposta(false);
+    setCorVerde(true);
+    setImagem(imagemCerto);
+    setContador(cartoesRespondidos);
+  }
+  return (
+    <>
+      {cartaoFechado &&
+        <PerguntaFechada>
+          <p>{perguntas.pergunta}</p>
+          <img onClick={() => showTheQuestion(index)} src={perguntas.autoplay}
+            alt={perguntas.alt} />
+        </PerguntaFechada>
+      }
 
-            {mostrarPergunta &&
-                <PerguntaAberta>
-                    <h1>{perguntas.Q}</h1>
-                    <img onClick={() => showTheAnswer(index)} src={perguntas.setinha}
-                        alt={perguntas.alt} />
-                </PerguntaAberta>
-            }
+      {mostrarPergunta &&
+        <PerguntaAberta>
+          <h1>{perguntas.Q}</h1>
+          <img onClick={() => showTheAnswer(index)} src={perguntas.setinha}
+            alt={perguntas.alt} />
+        </PerguntaAberta>
+      }
 
-            {mostrarResposta &&
-                <Resposta>
-                    <h1>{perguntas.R}</h1>
-                    <ContainerBotoes>
-                        <BotaoVermelho onClick={showCardAnswerRed}>Não<br />lembrei!</BotaoVermelho>
-                        <BotaoLaranja onClick={showCardAnswerOrange}>Quase não<br /> lembrei!</BotaoLaranja>
-                        <BotaoVerde onClick={showCardAnswerGreen}>Zap!</BotaoVerde>
-                    </ContainerBotoes>
-                </Resposta>
-            }
+      {mostrarResposta &&
+        <Resposta>
+          <h1>{perguntas.R}</h1>
+          <ContainerBotoes>
+            <BotaoVermelho onClick={showCardAnswerRed}>Não<br />lembrei!</BotaoVermelho>
+            <BotaoLaranja onClick={showCardAnswerOrange}>Quase não<br /> lembrei!</BotaoLaranja>
+            <BotaoVerde onClick={showCardAnswerGreen}>Zap!</BotaoVerde>
+          </ContainerBotoes>
+        </Resposta>
+      }
 
-            {corVermelho &&
-                <CartaoRespostaErrada>
-                    <p>{perguntas.pergunta}</p>
-                    <img src={perguntas.erro}
-                        alt={perguntas.alt} />
-                </CartaoRespostaErrada>
-            }
+      {corVermelho &&
+        <CartaoRespostaErrada>
+          <p>{perguntas.pergunta}</p>
+          <img src={perguntas.erro}
+            alt={perguntas.alt} />
+        </CartaoRespostaErrada>
+      }
 
-            {corLaranja &&
-                <CartaoRespostaQuase>
-                    <p>{perguntas.pergunta}</p>
-                    <img src={perguntas.quase}
-                        alt={perguntas.alt} />
-                </CartaoRespostaQuase>
-            }
+      {corLaranja &&
+        <CartaoRespostaQuase>
+          <p>{perguntas.pergunta}</p>
+          <img src={perguntas.quase}
+            alt={perguntas.alt} />
+        </CartaoRespostaQuase>
+      }
 
-            {corVerde &&
-                <CartaoRespostaCerto>
-                    <p>{perguntas.pergunta}</p>
-                    <img src={perguntas.certo}
-                        alt={perguntas.alt} />
-                </CartaoRespostaCerto>
-            }
+      {corVerde &&
+        <CartaoRespostaCerto>
+          <p>{perguntas.pergunta}</p>
+          <img src={perguntas.certo}
+            alt={perguntas.alt} />
+        </CartaoRespostaCerto>
+      }
 
-            <Rodape>
-                <p>{contador}/{index.length} CONCLUÍDOS</p>
-                <img src={imagem} alt={perguntas.alt} />
-            </Rodape>
+      <Rodape>
+        <p>{contador}/{index + 1} CONCLUÍDOS</p>
+        <img src={imagem[index + 1]} alt={perguntas.alt} />
+      </Rodape>
 
-        </>
-    )
+    </>
+  )
 }
 
 const PerguntaFechada = styled.div`
@@ -155,7 +155,7 @@ const PerguntaAberta = styled.div`
   justify-content: space-between;
   display: ${props => props.mostrarResposta ? "none" : ""};
   h1{
-    background-color:white;
+    background: #FFFFD5;
   }
   img{
   position: absolute;
@@ -183,14 +183,16 @@ const Resposta = styled.div`
   flex-direction: column;
   justify-content: space-between;
   h1{
-    background-color:white;
+    background: #FFFFD5;
   }
 `
-const ContainerBotoes = styled.span`
+const ContainerBotoes = styled.div`
   display: flex;
-  width: 80%;
+  width: 100%;
   justify-content: space-between;
-  margin: 20px;
+  margin: 2px;
+  padding: 15px;
+  background: #FFFFD5;
 `
 const BotaoVermelho = styled.button`
   width: 90px;
@@ -206,9 +208,9 @@ const BotaoVermelho = styled.button`
   color: #FFFFFF;
   background: #FF3030;
   border-radius: 5px;
-  border: 1px solid blue;
+  border: none;
   padding:5px;
-
+  margin: 5px;
 `
 const BotaoLaranja = styled.button`
  width: 90px;
@@ -224,8 +226,9 @@ const BotaoLaranja = styled.button`
   color: #FFFFFF;
   background: #FF922E;
   border-radius: 5px;
-  border: 1px solid blue;
+  border: none;
   padding:5px;
+  margin: 5px;
 `
 const BotaoVerde = styled.button`
  width: 90px;
@@ -241,8 +244,9 @@ const BotaoVerde = styled.button`
   color: #FFFFFF;
   background: #2FBE34;
   border-radius: 5px;
-  border: 1px solid blue;
+  border: none;
   padding:5px;
+  margin: 5px;
 `
 const CartaoRespostaErrada = styled.div`
   width: 300px;
@@ -262,8 +266,8 @@ const CartaoRespostaErrada = styled.div`
   font-size: 16px;
   line-height: 19px;
   color: ${props => props.corVermelho ? "black" : "#FF0000"};
-  background-color: white;
   text-decoration: ${props => props.corVermelho ? "none" : "line-through"};
+  background-color: #FFFFFF;
   }
   img{
     width:15px;
