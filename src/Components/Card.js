2b006e9e4dba370a7869389c8f4b43a5e68efe14
Sabta Mendes Alencar <sabtaamendes@gmail.com>
2022-09-28 13:50:01 -0400

    cartão com pergunta e com respostas e botões implementados

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,23 +1,44 @@
 import styled from "styled-components";
 import { useState } from "react";
 export default function Card({ perguntas, index }) {
+    const [cartaoFechado, setCartaoFechado] = useState(true);
     const [mostrarPergunta, setMostrarPergunta] = useState(false);
+    const [mostrarResposta, setMostrarResposta] = useState(false);
     function showTheQuestion(index) {
-        setMostrarPergunta(true)
+        setMostrarPergunta(true);
+        setCartaoFechado(false);
         console.log(perguntas.Q, index)
+    }
+    function showTheAnswer(index) {
+        console.log(perguntas.Q, index)
+        setMostrarResposta(true);
+        setMostrarPergunta(false);
+        setCartaoFechado(false);
     }
     return (
         <>
-            {mostrarPergunta
-                ? <PerguntaAberta>
-                    <h1>{perguntas.Q}</h1>
-                    <img src={perguntas.setinha} alt={perguntas.alt} />
-                </PerguntaAberta>
+            {cartaoFechado && <PerguntaFechada>
+                {perguntas.pergunta}
+                <img onClick={() => showTheQuestion(index)} src={perguntas.autoplay}
+                    alt={perguntas.alt} />
+            </PerguntaFechada>
+            }
 
-                : <PerguntaFechada>
-                    {perguntas.pergunta}
-                    <img onClick={() => showTheQuestion(index)} src={perguntas.autoplay} alt={perguntas.alt} />
-                </PerguntaFechada>
+            {mostrarPergunta && <PerguntaAberta>
+                <h1>{perguntas.Q}</h1>
+                <img onClick={() => showTheAnswer(index)} src={perguntas.setinha}
+                    alt={perguntas.alt} />
+            </PerguntaAberta>
+            }
+            
+            {mostrarResposta && <Resposta>
+                <h1>{perguntas.R}</h1>
+                <ContainerBotoes>
+                    <BotaoVermelho>Não<br />lembrei!</BotaoVermelho>
+                    <BotaoLaranja>Quase não<br /> lembrei!</BotaoLaranja>
+                    <BotaoVerde>Zap!</BotaoVerde>
+                </ContainerBotoes>
+            </Resposta>
             }
         </>
     )
@@ -34,7 +55,7 @@ const PerguntaFechada = styled.div`
   box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
   background-color: #FFFFFF;
   border-radius: 5px;
-  display: ${props => props.mostrarPergunta ? "none" : ""};
+  display: ${props => props.cartaoFechado ? "none" : ""};
   p{
   font-family: 'Recursive';
   font-style: normal;
@@ -69,6 +90,7 @@ const PerguntaAberta = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  display: ${props => props.mostrarResposta ? "none" : ""};
   h1{
     background-color:white;
   }
@@ -79,3 +101,83 @@ const PerguntaAberta = styled.div`
   background-color: white;
   }
   `
+const Resposta = styled.div`
+ width: 300px;
+  margin: 12px;
+  padding: 15px;
+  min-height: 100px;
+  background: #FFFFD5;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  font-family: 'Recursive';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 22px;
+  color: #333333;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  h1{
+    background-color:white;
+  }
+`
+const ContainerBotoes = styled.span`
+  display: flex;
+  width: 80%;
+  justify-content: space-between;
+  margin: 20px;
+`
+const BotaoVermelho = styled.button`
+  width: 90px;
+  font-family: 'Recursive';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: #FFFFFF;
+  background: #FF3030;
+  border-radius: 5px;
+  border: 1px solid blue;
+  padding:5px;
+
+`
+const BotaoLaranja = styled.button`
+ width: 90px;
+  font-family: 'Recursive';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: #FFFFFF;
+  background: #FF922E;
+  border-radius: 5px;
+  border: 1px solid blue;
+  padding:5px;
+`
+const BotaoVerde = styled.button`
+width: 90px;
+  font-family: 'Recursive';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: #FFFFFF;
+  background: #2FBE34;
+  border-radius: 5px;
+  border: 1px solid blue;
+  padding:5px;
+`
